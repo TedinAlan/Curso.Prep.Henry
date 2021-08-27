ffea4ea5be4e0ecfcf50fc5859565614ee2d385f
unknown <tedinalan@gmail.com>
2021-08-27 12:47:23 -0300

    terminado

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var array = [];
+  for(var prop in objeto) {
+    array.push([prop, objeto[prop]]);
+  }
+  return array;
 }
 
 
@@ -18,6 +23,19 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var obj = {};
+  for(var i = 0; i < string.length; i++ ) {
+   var count = 0;
+   if(!obj[string[i]]) {
+     for(var j = 0; j < string.length; j++) {
+       if(string[i] === string[j]) {
+         count++;
+       }
+     }
+     obj[string[i]] = count;
+   }
+  }
+  return obj;
 }
 
 
@@ -26,6 +44,16 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var textMax = "";
+  var textMin = "";
+  for(var i = 0; i < s.length; i++) {
+    if(s[i] === s[i].toUpperCase()) {
+      textMax = textMax + s[i];
+    } else {
+      textMin = textMin + s[i];
+    }
+  }
+  return textMax + textMin;
 }
 
 
@@ -35,6 +63,15 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var arrayStringInv = [];
+  var arrayPalabras = str.split(" ");
+
+  for(var i = 0; i < arrayPalabras.length; i++) {
+    var arrayLetras = arrayPalabras[i].split("");
+    var arrayLetrasInvertidas = arrayLetras.reverse();
+    arrayStringInv.push(arrayLetrasInvertidas.join(""))
+  }
+  return arrayStringInv.join(" ");
 } 
 
 
@@ -43,13 +80,30 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
-}
+  var string = numero.toString();
+  var arrayNum = string.split("");
+  var reverse = arrayNum.reverse();
+
+  if(string === reverse.join("")) {
+    return "Es capicua";
+  }else {
+   return "No es capicua";
+  }
+  }
+
 
 
 function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var cadenaNueva = "";
+  for(var i = 0; i < cadena.length; i++) {
+   if(cadena[i] !== "a" && cadena[i] !== "b" && cadena[i] !== "c") {
+    cadenaNueva += cadena[i]
+   }
+  }
+  return cadenaNueva;
 }
 
 
@@ -57,6 +111,19 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  /*for(var i = 0; i < arr.length; i++) {
+    for(var j = i + 1; j <arr.length; j++) {
+     if(arr[i].length > arr[j].length) {
+       var aux = arr[i];
+       arr[i] = arr[j];
+       arr[j] = aux;
+     }
+   }
+  }
+  return arr;*/
+  return arr.sort(function(a, b) {
+    return a.length - b.length;
+  })
 }
 
 
@@ -66,6 +133,22 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+ /* var matches = [];
+  for(var i = 0; i < arreglo1.length; i++) {
+    for(var j = 0; j < arreglo2.length; j++) {
+      if(arreglo1[i] === arreglo2[j]) {
+        matches.push(arreglo1[i]);
+      }
+    }
+  }
+  return matches;*/
+ return arreglo1.filter(function(el){
+   var aux = false;
+   arreglo2.forEach(function(e){
+     if ( el === e) aux = true;
+   })
+   return aux;
+ })
 }
 
 
